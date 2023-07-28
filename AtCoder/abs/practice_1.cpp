cf4020bdc855006ede3653bcbcde884d3bbd3afa
Ryoga-exe <49950206+Ryoga-exe@users.noreply.github.com>
2023-07-28 14:43:42 +0900

    [AtCoder] archive: abs/practice_1 #16660084

---
+++ AtCoder/abs/practice_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int a, b, c;
+  string s;
+  cin >> a >> b;
+  cin >> c;
+  cin >> s;
+  cout << a+b+c << " " << s << endl;
+  return 0;
+}
